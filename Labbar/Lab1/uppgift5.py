9a99e52228291fa9ec8562ce469706cca09f471f
Adam Jonsson <adamsmajl@gmail.com>
2018-01-19 08:02:44 +0100

    Update uppgift5.py

--- Labbar/Lab1/uppgift5.py
+++ Labbar/Lab1/uppgift5.py
@@ -1,12 +1,17 @@
 import sys, math
 
-mathValue = math.pi**2/6
-sumValue = 0
+def myFunction:
+    mathValue = math.pi**2/6
+    sumValue = 0
 
-n = int(input("\nn: "))
-for i in range(1, n + 1):
-    sumValue += 1/(i**2)
+    n = int(input("\nn: "))
+    for i in range(1, n + 1):
+        sumValue += 1/(i**2)
 
-print("Math value: ", mathValue)
-print("Sum value: ", sumValue)
-print("The diffrens between the values", (mathValue - sumValue), "\n")
+    print("Math value: ", mathValue)
+    print("Sum value: ", sumValue)
+    print("The diffrens between the values", (mathValue - sumValue), "\n")
+
+    userInput = input("Run again? y/n")
+    if(userInput == "y") myFunction()
+    else return true
