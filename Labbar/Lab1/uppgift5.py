d6a8b3fb34cba5bd3e6b6e9d97b680e6b1963586
Adam Jonsson <adamsmajl@gmail.com>
2018-01-23 11:14:03 +0100

    Update some changes

--- Labbar/Lab1/uppgift5.py
+++ Labbar/Lab1/uppgift5.py
@@ -5,12 +5,12 @@ def myFunction():
     sumValue = 0
 
     n = int(input("\nn: "))
-    for i in range(n):
+    for i in range(1, n + 1):
         sumValue += 1/(i**2)
 
     print("Math value: ", mathValue)
     print("Sum value: ", sumValue)
-    print("The difference between the valåäöues", (mathValue - sumValue), "\n")
+    print("The difference between the values", (mathValue - sumValue), "\n")
 
     userInput = input("Run again? y/n")
     if(userInput == "y"):
