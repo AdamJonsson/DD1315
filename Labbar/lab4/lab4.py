b10285aee79e8481933ab3f11a59cf9033ff6edc
Adam Jonsson <adamsmajl@gmail.com>
2018-03-07 07:55:19 +0100

    The P-tast

--- Labbar/lab4/lab4.py
+++ Labbar/lab4/lab4.py
@@ -16,9 +16,9 @@ class Account:
         return (accountOwner + accountMoney + self.print_history())
 
     def print_history(self):
-        accountTrasactions = "\tHistorik\n"
+        accountTrasactions = "Historik\n"
         for amountOfMoney in self.transactions: 
-            accountTrasactions += "\t\tTransaktion: " + str(amountOfMoney) + "sek\n"
+            accountTrasactions += "Transaktion: " + str(amountOfMoney) + "sek\n"
         return accountTrasactions
 
     def deposit(self, amount):
@@ -29,6 +29,7 @@ class Account:
     def withdrawal(self, amount, pin):
         if(self.ok_PIN(pin)):
             if(self.money > amount):
+                self.money -= amount
                 self.transactions.append(-amount)
                 return bcolors.OKGREEN + "Lyckades ta ut " + str(amount) + " ur ditt konto" + bcolors.ENDC
             else:
@@ -52,15 +53,15 @@ class PremiumAccount(Account):
     def withdrawal(self, amount, pin):
         if(self.ok_PIN(pin)):
             if(self.money > amount):
-                self.transactions.append(-amount)
-                return bcolors.OKGREEN + "Lyckades ta ut " + str(amount) + " ur ditt konto" + bcolors.ENDC
+                super().withdrawal(amount,pin)
             else:
-                return bcolors.WARNING+"Tyvärr finns inte tillräckligt med pengar. Du kanske vill ha ett lång?"+bcolors.ENDC
+                return bcolors.WARNING+"Tyvärr finns inte tillräckligt med pengar. Du kanske vill ha ett lån?"+bcolors.ENDC
         else:
             return bcolors.FAIL+"Fel PIN!"+bcolors.ENDC
 
-def find_account(accountName):
+def find_account():
     try:
+        accountName = input("Namn på konto: ") 
         account = account_dict[accountName]
         return account
     except KeyError:
@@ -95,45 +96,41 @@ def menu_choice():
 def execute(choice):
     if(choice == 1):
         print("\nSkapar ett nytt konto")
-        accountName = input("\tNamn på konto: ") 
-        accountMoney = get_int_input(input("\tPengar att lägga till: "))
-        accountPin = get_int_input(input("\tNya PIN: "))
+        accountName = input("Namn på konto: ") 
+        accountMoney = get_int_input(input("Pengar att lägga till: "))
+        accountPin = get_int_input(input("Nya PIN: "))
         account_dict[accountName] = Account(accountName, accountMoney, accountPin)
 
     elif(choice == 2):
         print("\nLägger till pengar")
-        accountName = input("\tNamn på konto: ") 
-        account = find_account(accountName)
+        account = find_account()
         if(account):
-            amountToAdd = get_int_input(input("\tMängd pengar: "))
+            amountToAdd = get_int_input(input("Mängd pengar: "))
             message = account.deposit(amountToAdd)
             print(message)
         
     elif(choice == 3):
         print("\nTa ut pengar")
-        accountName = input("\tNamn på konto: ") 
-        account = find_account(accountName)
+        account = find_account()
         if(account):
-            amountToTake = get_int_input(input("\tMängd pengar: "))
-            inputPin = get_int_input(input("\tPIN: "))
+            amountToTake = get_int_input(input("Mängd pengar: "))
+            inputPin = get_int_input(input("PIN: "))
             message = account.withdrawal(amountToTake, inputPin)
             print(message)
         
     elif(choice == 4):
         print("\nÄndra PIN")
-        accountName = input("\tNamn på konto: ") 
-        account = find_account(accountName)
+        account = find_account()
         if(account):
-            oldPin = get_int_input(input("\tNuvarande PIN: "))
-            newPin = get_int_input(input("\tNytt PIN: "))
+            oldPin = get_int_input(input("Nuvarande PIN: "))
+            newPin = get_int_input(input("Nytt PIN: "))
             message = account.change_PIN(oldPin, newPin)
             print(message)
 
     elif(choice == 5):
-        accountName = input("\tNamn: ") 
-        account = find_account(accountName)
+        account = find_account()
         if(account):
-            print(account_dict[accountName].print_history())
+            print(account.print_history())
     
     input("Tryck enter för att forsätta.\n")
 
@@ -141,19 +138,19 @@ def menu():
     userInput = 0
     while True:
         print("###################################")
-        print("Vad vill du göra?\n", "\t1 - Skapa ett nytt konto\n", "\t2 - Lägga in pengar\n", "\t3 - Ta ut pengar\n", "\t4 - Ändra PIN\n", "\t5 - Visa historik\n", "\t6 - Avlsuta")
+        print("Vad vill du göra?\n", "1 - Skapa ett nytt konto\n", "2 - Lägga in pengar\n", "3 - Ta ut pengar\n", "4 - Ändra PIN\n", "5 - Visa historik\n", "6 - Avlsuta")
         userInput = menu_choice()
-        if (userInput == 6 or userInput == "123"): 
+        if (userInput == 6): 
             break
         execute(userInput)
+
+def main():
+    menu()
 
 account_dict = {}
 account_dict["Lisa"] = Account("Lisa", 200, 1111)
 account_dict["Kalle"] = Account("Kalle", 100, 2222)
 account_dict["Kim"] = Account("Kim", 300, 3333)
 account_dict["Douglas"] = PremiumAccount("Douglas", 1000, 1234)
-def main():
-    menu()
-
 if __name__ == "__main__":
     main()
